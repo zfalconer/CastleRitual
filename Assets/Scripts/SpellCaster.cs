e614f89aea92dd4906f8752e83efcfa87fa53910
trokair <wandos1@yahoo.com>
2016-01-30 23:24:19 -0600

    Angel is now in with AI upgrades

--- Assets/Scripts/SpellCaster.cs
+++ Assets/Scripts/SpellCaster.cs
@@ -27,20 +27,45 @@ public class SpellCaster : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown (KeyCode.Mouse0) && spellTimer == 0) 
-		{
-			Spell1 ();
-		}
-
-		if (Input.GetKeyDown (KeyCode.Mouse1) && spellTimer == 0) 
-		{
-			Spell2 ();
-		}
+		//if there is a player tag us player input
+		if (spellTimer == 0 && this.transform.root.tag == "Player")
+			PlayerControls ();
+		else if (spellTimer == 0)
+			AIControls ();
+			
+			
 			
 		if (spellTimer > 0 && spellTimer < spellDelay1)
 			spellTimer += Time.fixedDeltaTime;
 		else
 			spellTimer = 0;
+	}
+
+	//input by a player person 
+	private void PlayerControls ()
+	{
+		if (Input.GetKeyDown (KeyCode.Mouse0)) 
+		{
+			Spell1 ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.Mouse1)) 
+		{
+			Spell2 ();
+		}
+	}
+
+	private void AIControls ()
+	{
+		switch (Random.Range (1, 3))
+		{
+			case 1:
+				Spell1 ();
+				break;
+			case 2:
+				Spell2 ();
+				break;
+		}
 	}
 
 	//this is for the light bolt
